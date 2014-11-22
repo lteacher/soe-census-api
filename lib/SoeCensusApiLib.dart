be3e5f2c78076e77b88a356903712f7f8b7f7d2a
Lessonteacher <hugo.armstrong@gmail.com>
2014-11-22 22:21:06 +1030

    Beginning Changes to SOE Census API

--- lib/SoeCensusApiLib.dart
+++ lib/SoeCensusApiLib.dart
@@ -3,10 +3,12 @@ library SoeCensusApiLib;
 
 // Set imports
 import 'dart:async';
-import "package:js/js.dart" as js;
+
+
+import "dart:js" as js;
 import "package:jsonp/jsonp.dart" as jsonp;
 import 'dart:convert';
 
 // Specify parts
-part 'SoeCensusQuery.dart';
-part 'SoeCensusCollection.dart';
+//part 'SoeCensusQuery.dart';
+//part 'SoeCensusCollection.dart';
